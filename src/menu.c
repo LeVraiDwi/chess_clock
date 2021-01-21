56f2a4fa3d99f686424f84c30e81ba8f929b7731
LeVraiDwi <67096994+LeVraiDwi@users.noreply.github.com>
2021-01-21 15:34:34 +0100

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -77,64 +77,40 @@ void	ft_menu(s_game *game)
 {
 	GtkWidget	*submenu;
 	GtkWidget	*menu_item;
-	GtkWidget	*menu;
 
 	game->menu_button = 0;
-	game->menu_button = gtk_menu_bar_new();
 	//bullet
-	menu = gtk_menu_new();
-	menu_item = gtk_menu_item_new_with_label("1 minute + 0 seconde");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_bullet_1), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("2 minutes + 1 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_bullet_2), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	//ajout de bullet
-	menu_item = gtk_menu_item_new_with_label("Bullet");
-	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), menu);
-	gtk_menu_shell_append(GTK_MENU_SHELL(game->menu_button), menu_item);
+	menu_item = gtk_button_new_with_label("1 minute + 0 seconde");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_bullet_1), (gpointer)game);
+	gtk_grid_attach(GTK_GRID(game->grid_custom), button, 1, 1, 1, 1);
+	menu_item = gtk_button_new_with_label("2 minutes + 1 seconde;s");
+	gtk_grid_attach(GTK_GRID(game->grid_custom), button, 1, 2, 1, 1);
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_bullet_2), (gpointer)game);
 	//blitz
 	menu = gtk_menu_new();
-	menu_item = gtk_menu_item_new_with_label("3 minutes + 0 seconde");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_blitz_1), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("3 minutes + 2 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_blitz_2), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("5 minutes + 0 seconde");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_blitz_3), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("5 minutes + 3 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_blitz_4), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	//ajout blitz
-	menu_item = gtk_menu_item_new_with_label("Blitz");
-	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), menu);
-	gtk_menu_shell_append(GTK_MENU_SHELL(game->menu_button), menu_item);
+	menu_item = gtk_button_new_with_label("3 minutes + 0 seconde");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_blitz_1), (gpointer)game);
+	menu_item = gtk_button_new_with_label("3 minutes + 2 secondes");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_blitz_2), (gpointer)game);
+	menu_item = gtk_button_new_with_label("5 minutes + 0 seconde");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_blitz_3), (gpointer)game);
+	menu_item = gtk_button_new_with_label("5 minutes + 3 secondes");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_blitz_4), (gpointer)game);
 	//rapid
 	menu = gtk_menu_new();
-	menu_item = gtk_menu_item_new_with_label("10 minutes + 0 seconde");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_rapid_1), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("10 minutes + 5 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_rapid_2), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("15 minutes + 10 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_rapid_3), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("30 minutes + 0 seconde");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_rapid_4), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	menu_item = gtk_menu_item_new_with_label("30 minutes + 20 secondes");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_rapid_5), (gpointer)game);
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
-	//ajout rapid
-	menu_item = gtk_menu_item_new_with_label("Rapid");
-	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), menu);
-	gtk_menu_shell_append(GTK_MENU_SHELL(game->menu_button), menu_item);
+	menu_item = gtk_button_new_with_label("10 minutes + 0 seconde");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_rapid_1), (gpointer)game);
+	menu_item = gtk_button_new_with_label("10 minutes + 5 secondes");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_rapid_2), (gpointer)game);
+	menu_item = gtk_button_new_with_label("15 minutes + 10 secondes");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_rapid_3), (gpointer)game);
+	menu_item = gtk_button_new_with_label("30 minutes + 0 seconde");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_rapid_4), (gpointer)game);
+	menu_item = gtk_button_new_with_label("30 minutes + 20 secondes");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_rapid_5), (gpointer)game);
 	//ajout custome
-	menu_item = gtk_menu_item_new_with_label("Custom");
-	g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(ft_custom), (gpointer)game);
+	/*menu_item = gtk_button_new_with_label("Custom");
+	g_signal_connect(G_OBJECT(menu_item), "clicked", G_CALLBACK(ft_custom), (gpointer)game);
 	game->popover = gtk_popover_new(menu_item);
-	gtk_menu_shell_append(GTK_MENU_SHELL(game->menu_button), menu_item);
+	gtk_menu_shell_append(GTK_MENU_SHELL(game->menu_button), menu_item);*/
 }
